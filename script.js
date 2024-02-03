9b8d4f41d980be3e07a39ceaa225df91af3bd911
Aishu1123 <aishlohakare2311@gmail.com>
2024-02-03 22:18:06 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,84 @@
+function submitForm() {
+  document.getElementById("contactForm").submit();
+  document.getElementById("contactForm").reset();
+}
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // sticky navbar on scroll script
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+
+    // scroll-up button show/hide script
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+
+  // slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
+  });
+
+  $(".navbar .menu li a").click(function () {
+    // applying again smooth scroll on menu items click
+    $("html").css("scrollBehavior", "smooth");
+  });
+
+  // toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+
+  // owl carousel script
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
+});
+
+//change the file here
+document.getElementById("resume-button-1").onclick = () => {
+  window.open(
+    "https://drive.google.com/file/d/1vGc8B8-U6i0CcZfq_IJpOrrClGBZ1ICV/view?usp=drive_link",
+    `_blank`
+  );
+};
+
+document.getElementById("resume-button-2").onclick = () => {
+  window.open(
+    "https://drive.google.com/file/d/1vGc8B8-U6i0CcZfq_IJpOrrClGBZ1ICV/view?usp=drive_link",
+    `_blank`
+  );
+};
+
+GitHubCalendar(".calendar", "kirti136", {
+  responsive: true,
+  global_stats: true,
+  tooltips: true,
+});
